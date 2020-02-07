bfd2224098b68ec01238ea723249f0d81b86d10b
Adrien Garrel <60453787+Rex7391@users.noreply.github.com>
2020-02-07 13:03:18 +0100

    Add files via upload

---
+++ Arete.java
@@ -0,0 +1,8 @@
+public class Arete extends Piece {
+
+	public Arete(Facelette facelette1, Facelette facelette2) {
+		this.facelette = new Facelette[2];
+		this.facelette[0] = facelette1;
+		this.facelette[1] = facelette2;
+	}
+}
---
+++ Cube.java
@@ -0,0 +1,45 @@
+public class Cube{
+
+	Face blanche = new Face(1,2,3,4,0);
+	Face bleue = new Face(5, 2,0, 4, 1);
+	Face rouge = new Face(5, 3,0, 1, 2);
+	Face verte = new Face(5, 4,0, 2, 3);
+	Face orange = new Face(5, 1,0, 3, 4);
+	Face jaune = new Face(2, 1,4, 3, 5);
+
+
+
+	Angle UFL = new Angle(new Facelette(0), new Facelette(1), new Facelette(2), 0);
+	Angle UBL = new Angle(new Facelette(0), new Facelette(2), new Facelette(3), 0);
+	Angle UBR = new Angle(new Facelette(0), new Facelette(3), new Facelette(4), 0);
+	Angle UFR = new Angle(new Facelette(0), new Facelette(4), new Facelette(1), 0);
+
+	Angle DFR = new Angle(new Facelette(5), new Facelette(0), new Facelette(4), 5);
+	Angle DBR = new Angle(new Facelette(5), new Facelette(4), new Facelette(3), 5);
+	Angle DBL = new Angle(new Facelette(5), new Facelette(3), new Facelette(2), 5);
+	Angle DFL = new Angle(new Facelette(5), new Facelette(2), new Facelette(1), 5);
+
+
+	Arete UF = new Arete(new Facelette(0), new Facelette(1), 0);
+	Arete UL = new Arete(new Facelette(0), new Facelette(2), 0);
+	Arete UB = new Arete(new Facelette(0), new Facelette(3), 0);
+	Arete UR = new Arete(new Facelette(0), new Facelette(4), 0);
+
+	Arete FR = new Arete(new Facelette(1), new Facelette(4), 1);
+	Arete FL = new Arete(new Facelette(1), new Facelette(2), 1);
+	Arete BL = new Arete(new Facelette(3), new Facelette(2), 3);
+	Arete BR = new Arete(new Facelette(3), new Facelette(4), 3);
+
+	Arete DR = new Arete(new Facelette(5), new Facelette(4), 5);
+	Arete DF = new Arete(new Facelette(5), new Facelette(1), 5);
+	Arete DL = new Arete(new Facelette(5), new Facelette(2), 5);
+	Arete DB = new Arete(new Facelette(5), new Facelette(3), 5);
+
+	Angle[] angles = {UFL, UBL, UBR, UFR, DFR, DBR, DBL, DFL};
+	Arete[] aretes = {UF, UL, UB, UR, FR, FL, BL, BR, DR, DF, DL, DB};
+
+	public Cube(){ //methode à completer avec l'etat actuel du cube récupéré sur l'affichage graphique
+	}
+
+
+}
