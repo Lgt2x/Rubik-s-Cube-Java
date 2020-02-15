b96d180563e1fa33b3fb20670dc00a079820f415
Louis Gombert <louisgombert@gmail.com>
2020-02-15 23:33:38 +0100

    Chargement correct des images

--- src/main/java/cubesolver/IHM/UXComponents/CommandeBoutons.java
+++ src/main/java/cubesolver/IHM/UXComponents/CommandeBoutons.java
@@ -9,16 +9,26 @@ import java.io.IOException;
 
 public class CommandeBoutons extends JPanel {
     //Déclaration de chaque bouton
-    private JButton pause  = new JButton(new ImageIcon("IHM/UXComponents/play2.png"));
-    private JButton avancer = new JButton(new ImageIcon("IHM/UXComponents/avancer.png"));
-    private JButton arriere  = new JButton(new ImageIcon("IHM/UXComponents/arriere.png"));
-    private Color maCouleur = new Color(48, 48, 48);
 
+    private Color maCouleur = new Color(48, 48, 48);
+    private JButton pause = new JButton();
+    private JButton avancer = new JButton();
+    private JButton arriere = new JButton();
 
     public CommandeBoutons(){
         this.setLayout(null);
         this.setBounds(1025, 10, 300, 75);
         this.setBackground(maCouleur);
+
+        // Chargement des images des boutons
+        // TODO : le faire dans des classes boutton séparées pour ne pas polluer ici
+        try {
+            pause.setIcon(new ImageIcon(ImageIO.read(getClass().getClassLoader().getResourceAsStream("BoutonsIHM/play2.png"))));
+            avancer.setIcon(new ImageIcon(ImageIO.read(getClass().getClassLoader().getResourceAsStream("BoutonsIHM/avancer.png"))));
+            arriere.setIcon(new ImageIcon(ImageIO.read(getClass().getClassLoader().getResourceAsStream("BoutonsIHM/arriere.png"))));
+        } catch (IOException e) {
+            System.out.println("Erreur de chargement d'une image");
+        }
 
         //Placement boutons
         arriere.setBounds(0, 0, 100, 75);
