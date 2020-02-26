6bf5c6d099da8f2be84924e58afebafdb9f774b1
adrien <lemaildadrien@yahoo.fr>
2020-02-26 13:45:23 +0100

    OLL version 2, il reste qq formules à completer

--- src/main/java/cubesolver/Solveur/OrientationOfLastLayer.java
+++ src/main/java/cubesolver/Solveur/OrientationOfLastLayer.java
@@ -5,134 +5,138 @@ import cubesolver.Cube.Cube;
 import java.util.HashMap;
 
 public class OrientationOfLastLayer {
-    int[][] cas = new int[4][3];
+    Long cas;
     Cube cube = new Cube();
     public OrientationOfLastLayer() {
-        int[][] t = new int[4][3];
 
-        HashMap<int[][], String> oll = new HashMap();
+        cas = conversionLastLayer();
+        int[][] t = new int[4][3];
+        boolean estResolu = false;
+
+        HashMap<Long, String> oll = new HashMap();
 
         //All Edges Oriented Correctly
-        oll.put(new int[][] {{0,0,1},{0,0,1},{0,0,0},{0,0,1}}, "RUUruRur"); //OCLL6
-        oll.put(new int[][] {{1,0,1},{0,0,0},{1,0,1},{0,0,0}}, "RUUruRUruRur"); //OCLL1
-        oll.put(new int[][] {{0,0,1},{0,0,0},{1,0,0},{0,0,0}}, ""); //OCLL4
-        oll.put(new int[][] {{1,0,1},{0,0,0},{0,0,0},{0,0,0}}, "RRDrUURdrUUr"); //OCLL3
-        oll.put(new int[][] {{1,0,0},{0,0,0},{1,0,0},{1,0,0}}, "RUrURUUr"); //OCLL7
-        oll.put(new int[][] {{1,0,0},{1,0,1},{0,0,1},{0,0,0}}, "RUURRuRRuRRUUR"); //OCLL2
-        oll.put(new int[][] {{0,0,1},{0,0,0},{0,0,0},{1,0,0}}, ""); //OCLL5
+        oll.put(001001000001L, "RUUruRur"); //OCLL6
+        oll.put(101000101000L, "RUUruRUruRur"); //OCLL1
+        oll.put(001000100000L, "URURDruRdRR"); //OCLL4
+        oll.put(101000000000L, "RRDrUURdrUUr"); //OCLL3
+        oll.put(100000100100L, "RUrURUUr"); //OCLL7
+        oll.put(100101001000L, "RUURRuRRuRRUUR"); //OCLL2
+        oll.put(001000000100L, "URRDrURdrur"); //OCLL5
 
         //T-Shapes
-        oll.put(new int[][] {{0,1,1},{0,0,0},{1,1,0},{0,0,0}}, "RUrurFRf"); //T1
-        oll.put(new int[][] {{0,1,0},{1,0,1},{0,1,0},{0,0,0}}, "FRUruf"); //T2
+        oll.put(011000110000L, "RUrurFRf"); //T1
+        oll.put(010101010000L, "FRUruf"); //T2
 
         // Squares
-        oll.put(new int[][] {{0,0,0},{1,1,0},{1,1,0},{1,0,0}}, ""); //S1
-        oll.put(new int[][] {{0,1,1},{0,1,1},{0,0,0},{0,0,1}}, ""); //S2
+        oll.put(000110110100L, "UUrFFLFlFR"); //S1
+        oll.put(011011000001L, "LFFrfRFl"); //S2
 
         //C-Shapes
-        oll.put(new int[][] {{0,1,0},{0,0,1},{0,1,0},{1,0,0}}, "RURRurFRURuf"); //C1
-        oll.put(new int[][] {{0,0,0},{0,1,0},{0,0,0},{1,1,1}}, "rurFRfUR"); //C2
+        oll.put(010001010100L, "RURRurFRURuf"); //C1
+        oll.put(000010000111L, "rurFRfUR"); //C2
 
         //W-Shapes
-        oll.put(new int[][] {{0,0,1},{0,0,0},{0,1,0},{1,1,0}}, "UURUrfRUrurFRurFRf"); //W1
-        oll.put(new int[][] {{0,1,0},{0,0,0},{1,0,0},{0,1,1}}, "RUrURururFRf"); //W2
+        oll.put(001000010110L, "UURUrfRUrurFRurFRf"); //W1
+        oll.put(010000100011L, "RUrURururFRf"); //W2
 
         //Corner Correct, Edges Flipped
-        oll.put(new int[][] {{0,1,0},{0,0,0},{0,0,0},{0,1,0}}, ""); //E1
-        oll.put(new int[][] {{0,1,0},{0,0,0},{0,1,0},{0,0,0}}, ""); //E2
+        oll.put(010000000010L, "LFrfRlURur"); //E1
+        oll.put(010000010000L, "RUruLrFRfl"); //E2
 
         //P-Shapes
-        oll.put(new int[][] {{0,1,1},{0,1,0},{1,0,0},{0,0,0}}, "ruFURurfR"); //P1
-        oll.put(new int[][] {{0,0,0},{0,0,0},{0,1,0},{1,1,1}}, "UrufUFR"); //P3
-        oll.put(new int[][] {{0,0,1},{0,1,0},{1,1,0},{0,0,0}}, "RUburURBr"); //P2
-        oll.put(new int[][] {{0,0,0},{1,1,1},{0,1,0},{0,0,0}}, "UUFURurf"); //P4
+        oll.put(011010100000L, "ruFURurfR"); //P1
+        oll.put(000000010111L, "UrufUFR"); //P3
+        oll.put(001010110000L, "RUburURBr"); //P2
+        oll.put(000111010000L, "UUFURurf"); //P4
 
         //I-Shapes
-        oll.put(new int[][] {{1,1,0},{1,0,1},{0,1,1},{0,0,0}}, ""); //I1
-        oll.put(new int[][] {{0,1,0},{1,0,1},{0,1,0},{1,0,1}}, ""); //I4
-        oll.put(new int[][] {{0,0,1},{0,1,0},{1,0,0},{1,1,1}}, ""); //I2
-        oll.put(new int[][] {{0,0,0},{1,1,1},{0,0,0},{1,1,1}}, ""); //I3
+        oll.put(110101011000L, ""); //I1
+        oll.put(010101010101L, ""); //I4
+        oll.put(001010100111L, ""); //I2
+        oll.put(000111000111L, ""); //I3
 
         //Knight Move Shapes
-        oll.put(new int[][] {{0,1,1},{0,0,1},{0,0,1},{0,1,0}}, "RUrurFRRUruf"); //F1
-        oll.put(new int[][] {{1,0,0},{1,0,0},{1,1,0},{0,1,0}}, "RUrUrFRfRuur"); //F2
-        oll.put(new int[][] {{0,0,1},{0,1,0},{0,1,0},{1,0,0}}, "RuurrFRfRuur"); //F3
-        oll.put(new int[][] {{0,1,1},{0,0,0},{0,0,0},{1,1,0}}, "FRuruRUrf"); //F4
+        oll.put(011001001010L, "RUrurFRRUruf"); //F1
+        oll.put(100100110010L, "RUrUrFRfRuur"); //F2
+        oll.put(001010010100L, "RuurrFRfRuur"); //F3
+        oll.put(011000000110L, "FRuruRUrf"); //F4
 
         //Awkward Shapes
-        oll.put(new int[][] {{0,0,0},{1,0,0},{0,1,0},{0,1,1}}, ""); //A1
-        oll.put(new int[][] {{1,1,0},{0,1,0},{0,0,1},{0,0,0}}, ""); //A2
-        oll.put(new int[][] {{0,1,0},{0,0,0},{1,0,1},{0,1,0}}, "RUrURuurFRUruf"); //A3
-        oll.put(new int[][] {{1,0,1},{0,0,0},{0,1,0},{0,1,0}}, "ruRurUURFRUruf"); //A4
+        oll.put(000100010011L, ""); //A1
+        oll.put(110010001000L, ""); //A2
+        oll.put(010000101010L, "RUrURuurFRUruf"); //A3
+        oll.put(101000010010L, "ruRurUURFRUruf"); //A4
 
         //L-Shapes
-        oll.put(new int[][] {{1,1,0},{1,0,1},{0,0,1},{0,1,0}}, "FRUruRUruf"); //L2
-        oll.put(new int[][] {{0,1,1},{0,1,0},{1,0,0},{1,0,1}}, "fluLUluLUF"); //L1
-        oll.put(new int[][] {{1,1,0},{1,1,1},{0,0,1},{0,0,0}}, ""); //L3
-        oll.put(new int[][] {{1,0,0},{1,1,1},{0,1,1},{0,0,0}}, ""); //L4
-        oll.put(new int[][] {{0,0,0},{1,1,1},{0,1,0},{1,0,1}}, ""); //L5
-        oll.put(new int[][] {{0,1,0},{1,1,1},{0,0,0},{1,0,1}}, ""); //L6
+        oll.put(110101001010L, "FRUruRUruf"); //L2
+        oll.put(011010100101L, "fluLUluLUF"); //L1
+        oll.put(110111001000L, ""); //L3
+        oll.put(100111011000L, ""); //L4
+        oll.put(000111010101L, ""); //L5
+        oll.put(010111000101L, ""); //L6
 
         //Lightning Bolts
-        oll.put(new int[][] {{1,1,0},{0,0,0},{1,0,0},{1,1,0}}, ""); //B1
-        oll.put(new int[][] {{0,0,1},{0,0,0},{0,1,1},{0,1,1}}, ""); //B2
-        oll.put(new int[][] {{1,0,0},{0,1,0},{1,1,0},{1,0,0}}, ""); //B3
-        oll.put(new int[][] {{0,1,1},{0,1,0},{0,0,1},{0,0,1}}, ""); //B4
-        oll.put(new int[][] {{0,1,0},{0,0,0},{1,1,0},{0,0,1}}, "LfluLUFul"); //B5
-        oll.put(new int[][] {{0,1,0},{1,0,0},{0,1,1},{0,0,0}}, "rFRUrufUR"); //B6
+        oll.put(110000100110L, ""); //B1
+        oll.put(001000011011L, ""); //B2
+        oll.put(100010110100L, ""); //B3
+        oll.put(011010001001L, ""); //B4
+        oll.put(010000110001L, "LfluLUFul"); //B5
+        oll.put(010100011000L, "rFRUrufUR"); //B6
 
         //No Edges Flipped Correctly
-        oll.put(new int[][] {{0,1,0},{1,1,1},{0,1,0},{1,1,1}}, "RuurrFRfuurFRf"); //O1
-        oll.put(new int[][] {{1,1,0},{1,1,1},{0,1,1},{0,1,0}}, ""); //O2
-        oll.put(new int[][] {{0,1,0},{1,1,0},{1,1,0},{1,1,0}}, ""); //O3
-        oll.put(new int[][] {{0,1,1},{0,1,1},{0,1,0},{0,1,1}}, ""); //O4
-        oll.put(new int[][] {{1,1,1},{0,1,0},{0,1,0},{0,1,0}}, ""); //O6
-        oll.put(new int[][] {{0,1,0},{1,1,0},{0,1,0},{0,1,1}}, ""); //O7
-        oll.put(new int[][] {{0,1,0},{1,1,0},{0,1,1},{0,1,0}}, "RUrUrFRfuurFRf"); //O5
-        oll.put(new int[][] {{0,1,0},{0,1,0},{0,1,0},{0,1,0}}, ""); //O8
+        oll.put(010111010111L, "RuurrFRfuurFRf"); //O1
+        oll.put(110111011010L, ""); //O2
+        oll.put(010110110110L, ""); //O3
+        oll.put(011011010011L, ""); //O4
+        oll.put(111010010010L, ""); //O6
+        oll.put(010110010011L, ""); //O7
+        oll.put(010110011010L, "RUrUrFRfuurFRf"); //O5
+        oll.put(010010010010L, ""); //O8
 
         do {
-
+            if(oll.containsKey(cas)) {
+                cube.formule(oll.get(cas));
+                estResolu = true;
+            }
             cube.mouvement('U');
-        }while(!estResolu(this.cas));
-
-
-
+        }while(!estResolu);
     }
 
-
-
-
-
-    public boolean comparateurCas(int[][] cas, int[][] solution){
-        boolean estIdentique = true;
-        for(int i = 0; i<solution.length; i++){
-            for(int j = 0; j<solution[i].length; j++){
-                if (cas[i][j] != solution[i][j]) {
-                    estIdentique = false;
-                    break;
-                }
-            }
-            if(!estIdentique){
-                break;
-            }
-        }
-        return estIdentique;
-    }
-
-    public boolean estResolu(int[][] cas){
-        boolean estResolu = true;
-        for(int i = 0; i<cas.length; i++){
-            for(int j = 0; j<cas[0].length; j++) {
-                if(cas[i][j] != 0){
-                    estResolu = false;
-                    break;
-                }
-            }
-            if(!estResolu){
-                break;
+    public Long conversionLastLayer() {
+        // l'objectif de cette methode est de convertir le cube en un long afin de connaitre l'orientation des pieces du dernier etage our resoudre la face avec une unique formule
+        long a = 0L;
+        long coef = 1L;
+        String voisins = "FLBR";
+        // les aretes jaune vont de 0 à 3, soit la boucle for :
+        for(int i = 0; i<4; i++){
+            if(cube.aretes[i].facelettes[0].face == 'U'){
+                // ne rien faire
+            }else if(cube.aretes[i].facelettes[0].face == 'F'){
+                a+=10L;
+            }else if(cube.aretes[i].facelettes[0].face == 'L'){
+                a+=10000L;
+            }else if(cube.aretes[i].facelettes[0].face == 'B'){
+                a+=10000000L;
+            }else{
+                a+=10000000000L;
             }
         }
-        return estResolu;
+        for(int i = 0; i<4; i++){
+            if(cube.angles[i].facelettes[0].face == 'U'){
+                //ne rien faire
+            } else{
+                int j = 0;
+                while(cube.angles[i].facelettes[0].face != voisins.charAt(j)){
+                    j++;
+                }
+                coef = (j+1)*1000L;
+                if(cube.angles[i].appartientFace(voisins.charAt(j+1))){
+                    coef *=100L;
+                }
+                a+=coef;
+            }
+        }
+        return a;
     }
 }
