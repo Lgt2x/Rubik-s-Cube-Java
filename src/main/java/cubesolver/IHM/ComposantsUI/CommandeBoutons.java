5e3cba57e7ff177c5df605dc034c113a1cf3ea8a
alex <alexandre.dumarquez@gmail.com>
2020-03-06 15:17:59 +0100

    Branchement des boutons avancer et arrière

--- src/main/java/cubesolver/IHM/ComposantsUI/CommandeBoutons.java
+++ src/main/java/cubesolver/IHM/ComposantsUI/CommandeBoutons.java
@@ -1,9 +1,15 @@
 package cubesolver.IHM.ComposantsUI;
 
+import cubesolver.Cube.Cube;
+import cubesolver.IHM.GestionAffichage;
+import cubesolver.Solveur.Solveur;
+
 import javax.swing.*;
 import java.awt.*;
 
 import javax.imageio.ImageIO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class CommandeBoutons extends JPanel {
@@ -13,11 +19,13 @@ public class CommandeBoutons extends JPanel {
     private JButton pause = new JButton();
     private JButton avancer = new JButton();
     private JButton arriere = new JButton();
+    private JLabel text = new JLabel();
+    private int niemeMouv = 0;
 
     public CommandeBoutons(){
         this.setLayout(null);
         this.setBounds(1025, 10, 300, 75);
-        this.setBackground(maCouleur);
+        //this.setBackground(maCouleur);
 
         // Chargement des images des boutons
         // TODO : le faire dans des classes boutton séparées pour ne pas polluer ici
@@ -31,7 +39,7 @@ public class CommandeBoutons extends JPanel {
 
         //Placement boutons
         arriere.setBounds(0, 0, 100, 75);
-        pause.setBounds(100, 0, 100, 75);
+        text.setBounds(100, 0, 100, 75);
         avancer.setBounds(200, 0, 100, 75);
 
         //Couleurs et Bordures
@@ -43,9 +51,49 @@ public class CommandeBoutons extends JPanel {
         pause.setBorder(BorderFactory.createLineBorder(Color.white));
         avancer.setBorder(BorderFactory.createLineBorder(Color.white));
 
-        this.add(arriere); this.add(avancer); this.add(pause);
+        avancer.addActionListener(new avancerListener());
+        arriere.addActionListener(new arriereListener());
+
+
+
+        this.add(arriere); this.add(avancer); this.add(pause); this.add(text);
 
 
 
     }
+
+    class avancerListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            niemeMouv++;
+            if(niemeMouv>GestionAffichage.formuleResolution.length){
+                niemeMouv--;
+            } else {
+                for(int i=0; i<=niemeMouv;i++){
+                    Cube.formule(GestionAffichage.formuleResolution[i]);
+                    GestionAffichage.actualise();
+                }
+            }
+            text.setText(String.valueOf(niemeMouv));
+        }
+    }
+
+    class arriereListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            niemeMouv--;
+            if(niemeMouv<0){
+                niemeMouv=0;
+            } else {
+                for(int i=0; i<=niemeMouv;i++){
+                    Cube.formule(GestionAffichage.formuleResolution[i]);
+                    GestionAffichage.actualise();
+                }
+            }
+            text.setText(String.valueOf(niemeMouv));
+        }
+    }
+
 }
