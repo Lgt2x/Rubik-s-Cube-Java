1b3d29e65b8ab0eeff4cd433f533cbff615dc22f
Louis Gombert <louisgombert@gmail.com>
2020-03-09 09:47:42 +0100

    Chargement correct des OLL

--- src/main/java/cubesolver/Solveur/Etapes/OrientationOfLastLayer.java
+++ src/main/java/cubesolver/Solveur/Etapes/OrientationOfLastLayer.java
@@ -3,12 +3,10 @@ package cubesolver.Solveur.Etapes;
 import cubesolver.Cube.Cube;
 import cubesolver.Solveur.EtapeResolution;
 import java.io.*;
-import java.util.ArrayList;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class OrientationOfLastLayer extends EtapeResolution {
     Long cas;
@@ -20,104 +18,18 @@ public class OrientationOfLastLayer extends EtapeResolution {
         cas = conversionLastLayer();
         boolean estResolu = false;
         HashMap<Long, String> oll = new HashMap();
-/*
-        //All Edges Oriented Correctly
-        oll.put(1001000001L, "RUUruRur"); //OCLL6
-        oll.put(101000101000L, "RUUruRUruRur"); //OCLL1
-        oll.put(1000100000L, "URURDruRdRR"); //OCLL4
-        oll.put(101000000000L, "RRDrUURdrUUr"); //OCLL3
-        oll.put(100000100100L, "RUrURUUr"); //OCLL7
-        oll.put(100101001000L, "RUURRuRRuRRUUR"); //OCLL2
-        oll.put(1000000100L, "URRDrURdrur"); //OCLL5
 
-        //T-Shapes
-        oll.put(11000110000L, "RUrurFRf"); //T1
-        oll.put(10101010000L, "FRUruf"); //T2
-
-        // Squares
-        oll.put(110110100L, "UUrFFLFlFR"); //S1
-        oll.put(11011000001L, "LFFrfRfl"); //S2
-
-        //C-Shapes
-        oll.put(10001010100L, "RURRurFRURuf"); //C1
-        oll.put(10000111L, "rurFRfUR"); //C2
-
-        //W-Shapes
-        oll.put(1000010110L, "UURUrfRUrurFRurFRf"); //W1
-        oll.put(10000100011L, "RUrURururFRf"); //W2
-
-        //Corner Correct, Edges Flipped
-        oll.put(10000000010L, "LFrfRlURur"); //E1
-        oll.put(10000010000L, "RUruLrFRfl"); //E2
-
-        //P-Shapes
-        oll.put(11010100000L, "ruFURurfR"); //P1
-        oll.put(10111L, "UrufUFR"); //P3
-        oll.put(1010110000L, "RUburURBr"); //P2
-        oll.put(111010000L, "UUFURurf"); //P4
-
-        //I-Shapes
-        oll.put(110101011000L, "UUFURurURurf"); //I1
-        oll.put(10101010101L, "lbLurURurURlBL"); //I4
-        oll.put(1010100111L, "RUrURuBubr"); //I2
-        oll.put(111000111L, "UrFRURuRRfRRurURUr"); //I3
-
-        //Fish Shapes
-        oll.put(11001001010L, "RUrurFRRUruf");//F1
-        oll.put(1010010100L, "RUURRFRfRUUr");//F3
-        oll.put(100100110010L, "RUrUrFRfRUUr");//F2
-        oll.put(11000000110L, "FRuruRUrf");//F4
-
-        //Knight Move Shapes
-        oll.put(110000110100L, "FURuRRfRURur"); //K1
-        oll.put(11001011000L, "rFRUrfRFuf"); //K2
-        oll.put(10100110100L, "lbLruRUlBL"); //K3
-        oll.put(11001010001L, "UURBrLUluRbr"); //K4
-
-        //Awkward Shapes
-        oll.put(100010011L, "URUruRurfuFRUr"); //A1
-        oll.put(110010001000L, "uFURUUruRUUruf"); //A2
-        oll.put(10000101010L, "RUrURuurFRUruf"); //A3
-        oll.put(101000010010L, "ruRurUURFRUruf"); //A4
-
-        //L-Shapes
-        oll.put(110101001010L, "FRUruRUruf"); //L2
-        oll.put(11010100101L, "fluLUluLUF"); //L1
-        oll.put(110111001000L, "ulUULUlULfluLUF"); //L3
-        oll.put(100111011000L, "uRUUruRurFRUruf"); //L4
-        oll.put(111010101L, "urfLflFLflFFR"); //L5
-        oll.put(10111000101L, "LFrFRfrFRFFl"); //L6
-
-        //Lightning Bolts
-        oll.put(110000100110L, "LFrFRFFl"); //B1
-        oll.put(1000011011L, "UUrfLflFFR"); //B2
-        oll.put(100010110100L, "lRRBrBRBBrBrL"); //B3
-        oll.put(11010001001L, "UFRUrufUFRUruf"); //B4
-        oll.put(10000110001L, "LfluLUFul"); //B5
-        oll.put(10100011000L, "rFRUrufUR"); //B6
-
-        //No Edges Flipped Correctly
-        oll.put(10111010111L, "RuurrFRfuurFRf"); //O1
-        oll.put(110111011010L, "FRUrufUUFURurf"); //O2
-        oll.put(10110110110L, "FRUrufufulULF"); //O3
-        oll.put(11011010011L, "UFRUrufUURUrURururFRf"); //O4
-        oll.put(111010010010L, "UUFRUrufurfLflFFR"); //O6
-        oll.put(10110010011L, "UUFRUrufuRUrfRUrurFRurFRf"); //O7
-        oll.put(10110011010L, "RUrUrFRfuurFRf"); //O5
-        oll.put(10010010010L, "FURurfUUrurFRfUR"); //O8
-
- */
-
-        //lire dans le fichier pour ajouter les oll
+        // Lire dans le fichier pour ajouter les oll
         try {
             String fileName = "BoutonsIHM/play2.png";
-            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-            File file = new File(classLoader.getResource(fileName).getFile());
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("Formules/positions.oll");
+            InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+            BufferedReader reader = new BufferedReader(streamReader);
 
-            BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
             String[] separateur;
-            while ((st = br.readLine()) != null) {
+            while ((st = reader.readLine()) != null) {
                 if (st.charAt(0) != '/' && st.charAt(1) != '/') {
                     separateur = st.split(" ");
                     oll.put(Long.parseLong(separateur[0]), separateur[1]);
